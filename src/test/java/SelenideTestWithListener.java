3705c79cd2cd3fe01d227bf00ddaab9f61ec6734
catcat4er <kerimov-t90@mail.ru>
2022-01-23 21:08:33 +0400

    added test with listeners

---
+++ src/test/java/SelenideTestWithListener.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTestWithListener {
+
+    @Test
+    public void testIssueSearch() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("catcat4er/AllureLesson");
+        $(".header-search-input").submit();
+
+        $(linkText("catcat4er/AllureLesson")).click();
+
+        $(partialLinkText("Issues")).click();
+
+        $(withText("#1")).should(Condition.visible);
+    }
+
+}
